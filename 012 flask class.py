a8ff891685a9a74fc3857f1e0391c40b53379be2
porosya <porosya@gmail.com>
2018-05-18 14:28:16 +0300

    012 flask class done

--- 012 flask class.py
+++ 012 flask class.py
@@ -2,11 +2,19 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/test')
-def home():
-    return 'hello world'
+
+@app.route('/<user>')
+def home(user):
+    return f'hello world, {user}'
 
 
+@app.route('/second/<first>/<second>')
+def home2(first, second):
+    return f'this is second task and result is {int(first) + int(second)}'
+
+@app.route('/third/<first>/<second>/<third>')
+def home3(first, second, third):
+    return f'this is second task and result is {max(first,second,third,key = len)}'
 
 
 if __name__ == '__main__':
